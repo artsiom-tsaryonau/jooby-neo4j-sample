2f8736645b5bc7004f7f8d9ba0c108541e8cda6e
Artsiom Tsaryonau <Artsiom_Tsaryonau@epam.com>
2018-10-15 16:27:42 +0300

    Minor demo refactoring

--- src/main/java/com/epam/project/Main.java
+++ src/main/java/com/epam/project/Main.java
@@ -1,22 +1,28 @@
 package com.epam.project;
 
-import com.epam.project.embed.MovieRepositoryApp;
-import com.epam.project.embed.PersonRepositoryApp;
+import com.epam.project.domain.Movie;
 import com.epam.project.module.Neo4jOGM;
 import org.jooby.Jooby;
 import org.jooby.json.Jackson;
+import org.neo4j.ogm.session.SessionFactory;
 
 public class Main extends Jooby {
 
-    {
-        use(new Neo4jOGM());
-        use(new Jackson());
-
-        use(new MovieRepositoryApp());
-        use("actors", new PersonRepositoryApp());
-    }
-
     public static void main(String[] args) {
-        run(Main::new, args);
+        var app = new Jooby().port(8888);
+        app.use(new Neo4jOGM());
+        app.use(new Jackson());
+        app.path("movies", () -> {
+           app.get(req -> {
+               var session = app.require(SessionFactory.class).openSession();
+               return session.loadAll(Movie.class);
+           });
+           app.get(":id", req -> {
+               var session = app.require(SessionFactory.class).openSession();
+               long value = req.param("id").longValue();
+               return session.load(Movie.class, value);
+           });
+        });
+        app.start();
     }
 }
